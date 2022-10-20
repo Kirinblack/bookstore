9ba07175b08289daf6dbaec957c14ccd688ca717
Lguilherme <masterextreme074@gmail.com>
2022-10-20 13:39:51 -0300

    refactor: update RentalService #12

--- src/main/java/com/gui/bookstore/service/RentalInterface.java
+++ src/main/java/com/gui/bookstore/service/RentalInterface.java
@@ -1,4 +1,18 @@
-//package com.gui.bookstore.service;
-//
-//public interface RentalInterface {
-//}
+package com.gui.bookstore.service;
+
+import com.gui.bookstore.model.RentalModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RentalInterface {
+
+    RentalModel create(RentalModel rentalModel);
+
+    RentalModel findById(Long id);
+
+    Page<RentalModel> findAll(Pageable pageable);
+
+    RentalModel update(Long id, RentalModel rentalModel);
+
+    void delete(Long id);
+}
